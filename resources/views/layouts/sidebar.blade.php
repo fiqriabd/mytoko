3fdf98e5116eed18ae89be2e7dc004a69b5c08f6
fiqriabd <fiqriabd1999!=@gmail.com>
2026-02-18 14:39:44 +0700

    menambahkan label

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,105 @@
+<!--begin::Sidebar-->
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand">
+        <!--begin::Brand Link-->
+        <a href="#" class="brand-link">
+            <!--begin::Brand Image-->
+            <img src="{{ asset('AdminLTE/dist/assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                class="brand-image opacity-75 shadow" />
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">My Toko</span>
+            <!--end::Brand Text-->
+        </a>
+        <!--end::Brand Link-->
+    </div>
+    <!--end::Sidebar Brand-->
+    <!--begin::Sidebar Wrapper-->
+    <div class="sidebar-wrapper">
+        <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="navigation"
+                aria-label="Main navigation" data-accordion="false" id="navigation">
+
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-speedometer"></i><p>Dashboard</p>
+                    </a>
+
+                </li>
+                <li class="nav-header">MASTER</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-box"></i><p>Kategori</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-boxes"></i><p>Produk</p>
+                    </a>
+
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-truck"></i><p>Distributor</p>
+                    </a>
+                    
+                </li>
+            
+                <li class="nav-header">TRANSAKSI</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-cash"></i><p>Pengeluaran</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-download"></i><p>Pembelian</p>
+                    </a>
+
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-upload"></i><p>Penjualan</p>
+                    </a>
+                    
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-cart-fill"></i><p>Transaksi Lama</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-cart-plus"></i><p>Transaksi Baru</p>
+                    </a>
+                </li>
+
+                <li class="nav-header">LAPORAN</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-file-earmark-pdf"></i><p>Laporan Keuangan</p>
+                    </a>
+                </li>
+            
+                <li class="nav-header">PENGATURAN</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-people"></i><p>Kasir</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-gear"></i><p>Pengaturan</p>
+                    </a>
+
+                </li>
+            
+            </ul>
+            <!--end::Sidebar Menu-->
+        </nav>
+    </div>
+    <!--end::Sidebar Wrapper-->
+</aside>
+<!--end::Sidebar-->
